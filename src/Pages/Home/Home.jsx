6435ab06e203594476e310bc1e65d24112799bff
deepaksingh <ds4850716@gmail.com>
2024-04-29 00:28:10 +0530

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "../../App.css";
+import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
+import RightSidebar from "../../components/RightSidebar/RightSidebar";
+import HomeMainbar from "../../components/HomeMainbar/HomeMainbar";
+
+const Home = ({ slideIn }) => {
+  const handleSlideIn = () => {
+
+  }
+  return (
+    <div className="home-container-1">
+      <LeftSidebar slideIn={slideIn} handleSlideIn={handleSlideIn} />
+      <div className="home-container-2">
+        <HomeMainbar />
+        <RightSidebar />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
